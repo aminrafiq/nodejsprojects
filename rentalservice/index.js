68eceaebbb2b3e4e737ff757c678b410d9129119
Amin Rafiq <amin.rafiq@tenpearls.com>
2022-11-03 17:32:20 +0500

    Db Changes

--- rentalservice/index.js
+++ rentalservice/index.js
@@ -7,16 +7,16 @@ const Rental = require('./models/rental');
 const path = require('path');
 
 const rentalRoutes = require('./routes/rentals'),
-      userRoutes = require('./routes/users'),
-      bookingRoutes = require('./routes/bookings'),
-      paymentRoutes = require('./routes/payments'),
-      imageUploadRoutes = require('./routes/image-upload'),
-      reviewRoutes = require('./routes/reviews');
+  userRoutes = require('./routes/users'),
+  bookingRoutes = require('./routes/bookings'),
+  paymentRoutes = require('./routes/payments'),
+  imageUploadRoutes = require('./routes/image-upload'),
+  reviewRoutes = require('./routes/reviews');
 
 mongoose.connect(config.DB_URI).then(() => {
   // if (process.env.NODE_ENV !== 'production') {
-    const fakeDb = new FakeDb();
-    // fakeDb.seedDb();
+  const fakeDb = new FakeDb();
+  // fakeDb.seedDb();
   // }
 });
 
@@ -36,13 +36,13 @@ if (process.env.NODE_ENV === 'production') {
   const appPath = path.join(__dirname, '..', 'dist');
   app.use(express.static(appPath));
 
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.resolve(appPath, 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT , function() {
+app.listen(PORT, function () {
   console.log('App is running!');
 });
